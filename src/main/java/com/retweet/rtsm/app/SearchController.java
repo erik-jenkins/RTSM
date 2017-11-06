efb0d48f6d4cc1e0d0f40c588b4f46915b206579
Erik Jenkins <ejenkins132@gmail.com>
2017-11-06 13:34:54 -0500

    add label for searching

--- src/main/java/com/retweet/rtsm/app/SearchController.java
+++ src/main/java/com/retweet/rtsm/app/SearchController.java
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.async.Callback;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.KeyCode;
@@ -17,6 +18,9 @@ public class SearchController {
 
     @FXML
     private ChoiceBox qualityExactly, qualityGreaterThan, qualityLessThan;
+
+    @FXML
+    private Label searchLabel;
 
     @FXML
     private TextField search, genus, recordist, country, loc, remarks, box, lat, lon, also,
@@ -43,22 +47,25 @@ public class SearchController {
 
     @FXML
     private void handleSearchButtonClick() {
+        searchLabel.setVisible(true);
         try {
             XenoCantoService.makeRequest(this.search.getText())
                     .asStringAsync(new Callback<String>() {
                         @Override
                         public void completed(HttpResponse<String> httpResponse) {
+                            searchLabel.setVisible(false);
                             System.out.println(httpResponse.getBody());
                         }
 
                         @Override
                         public void failed(UnirestException e) {
-
+                            searchLabel.setVisible(false);
+                            e.printStackTrace();
                         }
 
                         @Override
                         public void cancelled() {
-
+                            searchLabel.setVisible(false);
                         }
                     });
         } catch (UnirestException e) {
